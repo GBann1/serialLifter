479d609762351181d1fea9be2b71ecb6f3c99cd2
GBann <GTBannon@protonmail.com>
2024-04-08 02:08:58 -0600

    ensure same byte size

--- testing-zone/src/main.rs
+++ testing-zone/src/main.rs
@@ -7,11 +7,11 @@ fn read_data(port: &mut Box<dyn SerialPort>) -> Result<(), io::Error> {
     let mut reader = BufReader::new(port);
     let mut line = String::new();
     let mut stored_reading = String::new();
+
     loop {
-        if let Ok(_) = reader.read_line(&mut line){
-            if line != stored_reading {
+        if let Ok(_bytes) = reader.read_line(&mut line){
+            if line != stored_reading && _bytes == 16 {
                 println!("Data: {}", line);
-                io::stdout().flush().unwrap();
                 stored_reading = line.clone();
             }
             line.clear();
@@ -22,7 +22,7 @@ fn read_data(port: &mut Box<dyn SerialPort>) -> Result<(), io::Error> {
 fn main() {
     let port_name = "COM9"; // port for my computer only. change
     let baud_rate = 1200;
-    println!("Trying to connect on {} with {} baud.", &port_name, &baud_rate);
+    println!("Connecting on {} with {} baud.", &port_name, &baud_rate);
     if let Ok(mut port) = serialport::new(port_name, baud_rate)
         .timeout(Duration::from_millis(10))
         .open(){
